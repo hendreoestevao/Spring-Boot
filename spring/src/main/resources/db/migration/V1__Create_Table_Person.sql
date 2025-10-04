31cead680e38a3657294f78518ffd02724d774fa
hendreo santos <hendreosantos1@outlook.com>
2025-10-03 22:56:15 -0300

    add flyway

---
+++ spring/src/main/resources/db/migration/V1__Create_Table_Person.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `person` (
+                          `id` BIGINT(20) NOT NULL AUTO_INCREMENT,
+                          `first_name` VARCHAR(50) NOT NULL,
+                          `last_name` VARCHAR(50) NOT NULL,
+                          `address` VARCHAR(100) NOT NULL,
+                          `gender` VARCHAR(6) NOT NULL,
+                          PRIMARY KEY (`id`)
+);
---
+++ spring/src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,5 @@
+INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
+                                                                                (4, 'Brasil', 'Person Name0', 'Male', 'Person LastName0'),
+                                                                                (5, 'Brasil', 'Person Name0', 'Male', 'Person LastName0'),
+                                                                                (6, 'Brasil', 'Person Name0', 'Male', 'Person LastName0'),
+                                                                                (7, 'Brasil', 'Person Name0', 'Male', 'Person LastName0');
